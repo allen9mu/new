0dd283102c7faf7a21b78ee2460cd93ac3a9f8e4
allen9mu <xx@xx.com>
2017-09-05 21:32:09 +0800

    stage

--- computer/urls.py
+++ computer/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include,url
 from . import views
 
 urlpatterns =[
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.list_coputer, name='list_computer'),
 
 ]
--- computer/views.py
+++ computer/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render_to_response
-from models import Computer
+from django.shortcuts import render, get_object_or_404
+from .models import Computer
 
 # Create your views here.
 
 def list_coputer(request):
-    list = Computer.objects.
+    list = Computer.objects.order_by('date_added')
+    return render(request, 'computer/list.html',{'list':list})
+
+
+    
